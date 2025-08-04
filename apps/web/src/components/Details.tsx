e84e114eb46473f7b8b4b836c711390af3c83fc4
Sandro <sandro.gantze@icloud.com>
2025-08-04 23:01:20 +0200

    details content

--- apps/web/src/components/Details.tsx
+++ apps/web/src/components/Details.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 function Details() {
   return (
-    <main className='h-[80vh] w-full flex justify-center flex-col items-center'>
-        <h1>Lets Explain</h1>
-        <p>Why are we doing what we do and how does the software work ?</p>
+    <main className=' w-full flex justify-start flex-col items-center'>
+        <div className='w-full max-w-2xl  flex justify-start flex-col items-start'>
+        <h1 className='text-white text-[55px]'>Lets Explain</h1>
+        <p className='text-[20px] text-zinc-300 text-start'>Why are we doing what we do and how does the software work ?</p>
 
+        <p className='text-[18px] text-zinc-300 mt-5'>Why are we doing what we do and how does the software work there is more than just a few products and companys out there expacially for investor there is an overhaead complexity in keeping up with latest newes of companys of intrest to make a detailed valuation resaerch of a company there are multiple complex data facotrs requiered an typacilly it takes long time and invest to make a anlysis of a company we wanna change that process to help investors and analyst find a quicker way to get an overview of stocks and company with professional driven reasearch frameowrk form big players like BLackRock.</p>
+        </div>
+
+        
     </main>
   )
 }
